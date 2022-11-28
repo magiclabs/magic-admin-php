b5a73203d670966e8bbe8d15cbd0cfd065afb653
Justin Herrera <justin@magic.link>
2022-11-28 14:26:15 -0800

    Add wallet types

--- lib/Resource/User.php
+++ lib/Resource/User.php
@@ -16,21 +16,37 @@ class User
         $this->request_client = new \MagicAdmin\HttpClient($api_secret_key, $timeout, $retries, $backoff_factor);
     }
 
+    public function get_metadata_by_issuer_and_wallet($issuer, $wallet_type)
+    {
+        return $this->request_client->request('get', $this->v1_user_info, ['issuer' => $issuer, 'wallet_type' => $wallet_type]);
+    }
+    
     public function get_metadata_by_issuer($issuer)
     {
-        return $this->request_client->request('get', $this->v1_user_info, ['issuer' => $issuer]);
+        return $this->get_metadata_by_issuer_and_wallet($issuer, Wallet::NONE);
+    }
+
+    public function get_metadata_by_public_address_and_wallet($public_address, $wallet_type)
+    {
+        return $this->get_metadata_by_issuer(
+            \MagicAdmin\Util\DidToken::construct_issuer_with_public_address($public_address),
+            $wallet_type,
+        );
     }
 
     public function get_metadata_by_public_address($public_address)
     {
-        return $this->get_metadata_by_issuer(
-            \MagicAdmin\Util\DidToken::construct_issuer_with_public_address($public_address)
-        );
+        return $this->get_metadata_by_public_address_and_wallet($public_address, Wallet::NONE);
+    }
+
+    public function get_metadata_by_token_and_wallet($did_token, $wallet_type)
+    {
+        return $this->get_metadata_by_issuer($this->token->get_issuer($did_token), $wallet_type);
     }
 
     public function get_metadata_by_token($did_token)
     {
-        return $this->get_metadata_by_issuer($this->token->get_issuer($did_token));
+        return $this->get_metadata_by_token_and_wallet($did_token, Wallet::NONE);
     }
 
     public function logout_by_issuer($issuer)
